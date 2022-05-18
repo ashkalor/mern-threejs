a57906b17717f5b60a7edb60e3f0cd2077ed72d3
AshKalor <akhibkalnoor@gmail.com>
2022-05-18 20:27:23 +0530

    Setup Error Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,18 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Register a user
 // @access  Private
 // @route   POST /api/register
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Register User" });
-};
+});
 
 // @desc    Login user
 // @access  Private
 // @route   POST /api/login
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Login User" });
-};
+});
 
 module.exports = {
   registerUser,
