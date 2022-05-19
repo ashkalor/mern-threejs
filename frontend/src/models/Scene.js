a9ffdf7e2f96ff0902f671626926b9e041ec0bd1
AshKalor <akhibkalnoor@gmail.com>
2022-05-20 04:20:54 +0530

    Add a better scene in dashboard

--- frontend/src/models/Scene.js
+++ frontend/src/models/Scene.js
@@ -12,7 +12,7 @@ import { useGLTF } from "@react-three/drei";
 export default function Model({ ...props }) {
   const group = useRef();
   const { nodes, materials } = useGLTF("/scene.gltf");
-  console.log(nodes);
+
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,15 @@
-import { useEffect, Suspense } from "react";
+import { useEffect, Suspense, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Canvas } from "@react-three/fiber";
-import Model from "../models/Scene";
 import {
-  OrbitControls,
   Environment,
-  Backdrop,
-  ContactShadows,
+  MeshReflectorMaterial,
+  OrbitControls,
+  PerspectiveCamera,
 } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import Spinner from "../components/Spinner";
+import Model from "../models/Scene";
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
@@ -19,53 +20,114 @@ const Dashboard = () => {
     }
   }, [user, navigate]);
 
+  function Rotate(props) {
+    const ref = useRef();
+    useFrame((state) => (ref.current.rotation.y = state.clock.elapsedTime));
+    return <group ref={ref} {...props} />;
+  }
+
   return (
     <>
-      <Canvas
-        style={{ background: "black" }}
-        dpr={[1, 2]}
-        shadows
-        camera={{
-          fov: 75,
-          near: 1,
-          far: 1000,
-        }}
-      >
-        <ambientLight intensity={0.2} />
-        <directionalLight position={[-10, 0, -5]} intensity={1} color="red" />
-        <directionalLight
-          position={[-1, -2, -5]}
-          intensity={0.2}
-          color="#0c8cbf"
-        />
-        <spotLight
-          position={[5, 0, 5]}
-          intensity={2.5}
-          penumbra={1}
-          angle={0.35}
-          castShadow
-          color="#0c8cbf"
-        />
-        <Suspense fallback={null}>
-          <Model position={[0, -4, -1]} scale={0.003} />
-        </Suspense>
-        <Backdrop
-          castShadow
-          floor={2}
-          position={[0, -7.5, -13]}
-          scale={[100, 800, 40]}
+      <Suspense fallback={<Spinner />}>
+        <Canvas
+          gl={{ alpha: false }}
+          dpr={[1, 1.5]}
+          camera={{ fov: 70, position: [0, 2, 15] }}
+          shadows
         >
-          <meshStandardMaterial color="#353540" envMapIntensity={0.1} />
-        </Backdrop>
-        <ContactShadows
-          position={[0, -0.485, 0]}
-          scale={5}
-          blur={1.5}
-          far={1}
-        />
-        <Environment preset="city" />
-        <OrbitControls makeDefault />
-      </Canvas>
+          <color attach="background" args={["#191920"]} />
+          <fog attach="fog" args={["#191920", 0, 15]} />
+          <Environment preset="city" />
+          <OrbitControls
+            makeDefault
+            autoRotate
+            autoRotateSpeed={0.3}
+            maxPolarAngle={Math.PI / 2.3}
+            minPolarAngle={Math.PI / 2.3}
+            enableZoom={false}
+            enablePan={false}
+            enableDamping={true}
+          />
+          <group position={[0, -0.5, 0]}>
+            <Rotate>
+              <Model scale={0.0007} />
+            </Rotate>
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 11]}>
+              <planeGeometry args={[50, 50]} />
+              <MeshReflectorMaterial
+                blur={[400, 100]}
+                resolution={2048}
+                mixBlur={0}
+                mixStrength={11}
+                mixContrast={1}
+                mirror={1}
+                depthToBlurRatioBias={1}
+                roughness={100}
+                depthScale={1.4}
+                minDepthThreshold={1.2}
+                maxDepthThreshold={1.4}
+                color="#101010"
+                metalness={0.5}
+              />
+            </mesh>
+          </group>
+          <group position={[2, -0.5, -1]}>
+            <Model scale={0.0007} />
+
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 11]}>
+              <planeGeometry args={[50, 50]} />
+              <MeshReflectorMaterial
+                blur={[400, 100]}
+                resolution={2048}
+                mixBlur={0}
+                mixStrength={11}
+                mixContrast={1}
+                mirror={1}
+                depthToBlurRatioBias={1}
+                roughness={100}
+                depthScale={1.4}
+                minDepthThreshold={1.2}
+                maxDepthThreshold={1.4}
+                color="#101010"
+                metalness={0.5}
+              />
+            </mesh>
+          </group>
+          <group position={[-2, -0.5, -1]}>
+            <Model scale={0.0007} />
+
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 11]}>
+              <planeGeometry args={[50, 50]} />
+              <MeshReflectorMaterial
+                blur={[400, 100]}
+                resolution={2048}
+                mixBlur={0}
+                mixStrength={11}
+                mixContrast={1}
+                mirror={1}
+                depthToBlurRatioBias={1}
+                roughness={100}
+                depthScale={1.4}
+                minDepthThreshold={1.2}
+                maxDepthThreshold={1.4}
+                color="#101010"
+                metalness={0.5}
+              />
+            </mesh>
+          </group>
+
+          <PerspectiveCamera makeDefault fov={65} position={[0, 0, 4]}>
+            <spotLight
+              position={[0, 10, 0]}
+              angle={0.1}
+              penumbra={2}
+              intensity={5}
+              castShadow
+              shadow-mapSize={[2048, 2048]}
+            />
+          </PerspectiveCamera>
+        </Canvas>
+      </Suspense>
     </>
   );
 };
