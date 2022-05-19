9c9c09d419f0f7c2a8cc991bd8d6fec8787a7145
AshKalor <akhibkalnoor@gmail.com>
2022-05-19 23:35:19 +0530

    Serve frontend in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
-
+const path = require("path");
 const connectDB = require("./config/db");
+
 connectDB();
 
 const app = express();
@@ -15,6 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
